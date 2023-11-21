7e99ea942f123304680c363341400cc829b4f5ac
Minasuki <jossiel2023@gmail.com>
2023-11-21 01:43:32 -0700

    Creando la clase images

---
+++ src/estado/images.js
@@ -0,0 +1,78 @@
+import bulbasaur from './img/bulbasaur.svg'
+import charter from './img/charcter.svg'
+import eevee from './img/eevee.svg'
+import egg from "./img/egg.svg";
+import go from "./img/go.svg";
+import jigglypuff from "./img/jigglypuff.svg";
+import meowth from "./img/meowth.svg";
+import pidgey from "./img/pidgey.svg";
+import pikachu from "./img/pikachu.svg";
+import poke_rocket from "./img/poke_rocket.svg";
+import pokemon from "./img/pokemon.svg";
+import psyduck from "./img/psyduck.svg";
+import rattata from "./img/rattata.svg";
+
+const imgs = [
+  {
+    id: 1,
+    img: bulbasaur,
+    alt: 'bulbasaur',
+  },
+  {
+    id: 2,
+    img: charter,
+    alt: 'charter',
+  },
+  {
+    id: 3,
+    img: eevee,
+    alt: 'eevee',
+  },
+  {
+    id: 4,
+    img: egg,
+    alt: 'egg',
+  },
+  {
+    id: 5,
+    img: go,
+    alt: 'go',
+  },
+  {
+    id: 6,
+    img: jigglypuff,
+    alt: 'jigglypuff',
+  },
+  {
+    id: 7,
+    img: meowth,
+    alt: 'meowth',
+  },
+  {
+    id: 8,
+    img: pidgey,
+    alt: 'pidgey',
+  },
+  {
+    id: 9,
+    img: pikachu,
+    alt: 'pikachu',
+  },
+  {
+    id: 10,
+    img: poke_rocket,
+    alt: 'poke_rocket',
+  },
+  {
+    id: 11,
+    img: rattata,
+    alt: 'rattata',
+  },
+  {
+    id: 12,
+    img: psyduck,
+    alt: 'psyduck',
+  },
+]
+
+export default imgs
